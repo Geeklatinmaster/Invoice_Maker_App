fae2f178eed2144f282a930ac4d54f5627dd4fce
Geeklatinmaster <geeklatinmaster@gmail.com>
2025-09-09 09:20:04 -0400

    fix: remove i18n import from main.tsx for clean hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 import './index.css'
-import './i18n' // Initialize i18next
 
 createRoot(document.getElementById('root')!).render(<App />)
